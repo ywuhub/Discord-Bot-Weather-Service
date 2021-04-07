17dca3f64a8db95d0d4c665a9962936b2885ed4a
Allan Wu <allan.wu6@gmail.com>
2021-04-07 22:23:37 +1000

    Removed some testing data

--- weather_api.py
+++ weather_api.py
@@ -5,13 +5,13 @@ import os
 import requests
 
 # Methods to fetch from Weather API
+# Get the current weather for an Australia city
 def currentWeather(location):
     city = location
     country = "Australia"
     place = city + "," + country
     url = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric" % (place, apikey)
     response = requests.get(url)
-    print(response.text)
     data = json.loads(response.text)
     returnedCountry = data['sys']['country']
     if returnedCountry != "AU":
@@ -20,6 +20,10 @@ def currentWeather(location):
     else:
         return data
 
+# Get the 5 day forecast (3 hour intervals) for an Australia city
+def forecastWeather(location):
+    # TODO:
+    return
+
 load_dotenv(".env", verbose=True)
 apikey = os.getenv('APIKEY')
-currentWeather('Sydney')
