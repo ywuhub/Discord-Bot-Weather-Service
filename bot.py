8fad8e879ed8cea4f1d7160383bee46403e23ff3
patchangg <patrrynnn@hotmail.com>
2021-04-14 22:53:24 +1000

    Try and Catch
    Added Try and Catch clauses when looking up weather information for the city the user inputted.

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 import discord
 from dotenv import load_dotenv
 import os
-from process_commands import get_current_weather,get_forecast_weather, help_menu
+from process_commands import get_current_weather, help_menu, get_forecast_weather
 
 # Initialise Client Connection to Discord
 client = discord.Client()
@@ -10,6 +10,7 @@ client = discord.Client()
 # Listen for Discord Events
 @client.event
 async def on_message(message):
+    # ignore discord chat message if the author of the message was the bot
     if message.author == client.user:
         return
 
@@ -21,6 +22,11 @@ async def on_message(message):
         await get_forecast_weather(msg,message)
     elif message.content.startswith('$help'):
         await help_menu(message)
+
+@client.event
+async def on_ready():
+   channel = client.get_channel(828964390941753366)
+   await channel.send("The weather service bot is online. Please type $help for more a list of all the options.")
 
 # Load Environmental Variables
 load_dotenv(".env", verbose=True)
