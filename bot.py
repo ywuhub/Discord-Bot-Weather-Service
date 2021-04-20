ac081ec84f1b96c913ffd1cc6c3709f5e8818094
Allan Wu <allan.wu6@gmail.com>
2021-04-20 22:17:49 +1000

    Progress: Beginning the Bot Settings implementation

--- bot.py
+++ bot.py
@@ -8,6 +8,11 @@ from weather_api import currentWeather,forecastWeather
 load_dotenv(".env", verbose=True)
 TOKEN = os.getenv('TOKEN')
 
+# Bot Configuration
+config = {
+    'units': 'degrees'
+}
+
 # Initialise Client Connection to Discord
 bot = commands.Bot(command_prefix='!')
 bot.remove_command('help')
@@ -39,10 +44,20 @@ Weather Service Discord Bot - Help Menu
 """
     await ctx.send(response)
 
+@bot.command(name='settings')
+async def bot_settings(ctx):
+    response = """
+Weather Service Discord Bot - Settings
+To change these settings please type !setting [value].
+To see all values for each setting type !setting all
+Settings:
+    """
+    await ctx.send(response)
+
 @bot.event
 async def on_ready():
     print('Ready!')
     channel = bot.get_channel(828964390941753366)
-    await channel.send('The weather service bot is online. Please type $help for more a list of all the options.')
+    await channel.send('The weather service bot is online. Please type !help for more a list of all the options.')
 
 bot.run(TOKEN)
